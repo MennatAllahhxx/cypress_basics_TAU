cb546dac61d1e67605ff5f775640a8d1eb26e681
Menna Hassan <h.menna85@gmail.com>
2024-11-26 02:43:41 +0200

    adding assertion tests

--- cypress/e2e/specs.cy.js
+++ cypress/e2e/specs.cy.js
@@ -1,3 +1,5 @@
+/// <reference types='cypress'/>
+
 it('create a new list, new card, delete a list', () => {
     cy.visit("/board/1");
     cy.get('[data-cy="add-list-input"]')
@@ -10,9 +12,38 @@ it('create a new list, new card, delete a list', () => {
       .click();
 });
 
-it.only('favourite this board', () => {
+it('favourite this board', () => {
   cy.visit('/');
   cy.get('.star')
     .first()
     .click({force: true});
 });
+
+it.only('making assertions', () => {
+  cy.visit('/board/1');
+
+  cy.get('[data-cy="new-card"]')
+    .click();
+  cy.get('[data-cy="new-card-input"]')
+    .type('bread{enter}');
+  cy.get('[data-cy="new-card-input"]')
+    .type('milk{enter}');
+
+  cy.get('[data-cy="card"]')
+    .should("be.visible");
+  cy.get('[data-cy="card"]')
+    .should("have.length", 2);
+
+  cy.get('[data-cy="card-checkbox"]')
+    .first()
+    .check();
+  cy.get('[data-cy="due-date"')
+    .first()
+    .should('have.class','completed');
+  
+  cy.get('[data-cy="list-name"')
+    .should('have.value','groceries');
+  cy.get('[data-cy="card-text"')
+    .first()
+    .should('have.text','bread');
+})
