5d6b95429e07ac9471c967c76c7bf364c16187fd
Menna Hassan <h.menna85@gmail.com>
2024-11-26 05:59:27 +0200

    stubbing using body

--- cypress/e2e/stubbing_start.cy.ts
+++ cypress/e2e/stubbing_start.cy.ts
@@ -3,7 +3,13 @@ it.only('getting a list of boards', () => {
         method: 'GET',
         url: '/api/boards'
     }, {
-        fixture: 'twoBoards.json'
+        body: [{
+            name: 'TAU',
+            starred: true,
+            id: 11,
+            user: 0,
+            created: '2024-11-26'
+        }]
     }).as('boardList');
 
     cy.visit('/');
